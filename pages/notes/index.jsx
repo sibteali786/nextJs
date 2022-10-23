d14bbe68036e79e90a0d12d7cc447a88196ec4db
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2022-10-23 14:10:31 +0500

    Routing navigation 1. Added next Link example and how it works. 2. Single url vs Multiple url under same route. 3. how to correclty use next/link with or without anchor "a" tag.

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-  return <div>Notes Index path</div>;
+  return (
+    <div>
+      <h1> Notes Index path</h1>
+      <Link href="/notes/[id]" as="/notes/1">
+        <a>Note 1</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Page;
