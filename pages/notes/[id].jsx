d8905b660de02ac02275a382f3f4a368b38f81e5
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2022-11-04 20:03:55 +0500

    data fetching topic

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,17 +1,33 @@
 // pages/notes/index.jsx
 import React from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-const Page = () => {
-  const router = useRouter()
-  const { id }= router.query
 
+const Page = ({note}) => {
   return (
     <div sx={{variant: 'containers.page'}}>
-      <h1>Note: {id} </h1>
+      <h1>Note: {note.title} </h1>
     </div>
   )
 }
 
+export async function getServerSideProps({params, req, res}) {
+  const response = await fetch(`http://localhost:3000/api/note/${params.id}`)
+
+  // so much power!
+  if (!response.ok) {
+    res.setHeader( 'Location', '/notes' )
+    res.statusCode = 302;
+    res.end();
+    return {props: {}}
+  }
+
+  const {data} = await response.json()
+  
+  if (data) {
+    return {
+      props: {note: data}
+    }
+  }
+}
 
 export default Page;
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -3,13 +3,11 @@
 import { jsx } from 'theme-ui'
 import Link from 'next/link'
 
-const Page = () => {
-  const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `This is my note ${i}`}))
+const Page = ({notes}) => {
 
   return (
     <div sx={{variant: 'containers.page'}}>
       <h1>My Notes</h1>
-
       <div sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap'}}>
         {notes.map(note => (
           <div sx={{width: '33%', p: 2}}>
@@ -28,3 +26,13 @@ const Page = () => {
 }
 
 export default Page; 
+
+export async function getServerSideProps () {
+  const res = await fetch('http://localhost:3000/api/note');
+  const {data} = await res.json();
+
+  console.log(data);
+  return {  
+    props: {notes: data}
+  }
+}
